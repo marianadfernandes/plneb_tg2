051684b189fae120b126cfdf9f290dcb5454ec98
Mariana Fernandes <a92048@alunos.uminho.pt>
2023-05-29 19:26:13 +0100

    novo dicionario de categorias

--- categorize.py
+++ categorize.py
@@ -5,8 +5,11 @@ from nltk.corpus import wordnet
 with open('./ficheiros/dicionario_final.json', 'r', encoding="utf8") as file:
     dic = dict(json.load(file))
 
+new_dic = {}
 # Get synsets for the word
 for word in dic.keys():
+    print("----")
+    print("Key:", word)
     # Print synonyms for each synset and their categories
     synsets = wordnet.synsets(word, lang='por')  # Specify the language as 'por' for Portuguese
 
@@ -22,5 +25,15 @@ for word in dic.keys():
         print("Hypernyms:")
         for hypernym in hypernyms:
             print(hypernym.name())
+
+            hypernym_name = hypernym.name().split('.')[0]
+            if hypernym_name in new_dic:
+                new_dic[hypernym_name].append({word:dic[word]})
+            else:
+                new_dic[hypernym_name] = [{word:dic[word]}]
+
+file = open("output/dic_categorias.json","w", encoding="utf8")
+json.dump(new_dic,file, ensure_ascii=False, indent = 4)
+file.close()
+
         
-        print("----")
